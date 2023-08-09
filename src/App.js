796739851410b2ee07181ea033104e8777e34e06
Hridoy <ridoy.java@gmail.com>
2023-08-09 15:53:15 +0600

    react-spinner

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { BarLoader, BounceLoader, BeatLoader } from "react-spinners";
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BounceLoader loading size={30} color="red" />
+      <BarLoader loading size={20} color="green" />
+      <BeatLoader loading size={10} color="blue" />
     </div>
   );
 }
